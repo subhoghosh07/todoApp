daede7253c33e9a3c4ba1ee52d0beb51a1cf3039
subhoghosh07 <100135982+subhoghosh07@users.noreply.github.com>
2022-05-21 14:37:40 +0530

    src/components

---
+++ src/components/CreateTask.js
@@ -0,0 +1,58 @@
+import React, {useRef,useEffect} from 'react';
+import {Form,Input} from 'antd';
+import {DownOutlined } from '@ant-design/icons'
+
+const CreateTask = (
+    { allTasks,completeAllTasks,addTask }
+    ) => {
+    const [form] = Form.useForm();
+    const inputRef = useRef(null);
+    
+    useEffect(() => {
+        if(allTasks.length >0)
+        inputRef.current.focus();
+      }, [allTasks]);
+
+    //form submit
+    const handleSubmit = (e)=> {
+        form
+        .validateFields()
+        .then((values) => {
+            if (!values.textData) return;
+            addTask(values.textData);
+            form.resetFields();
+        })
+        .catch((err) => {
+          console.error("error", err);
+        });
+    }
+    return (
+        <>
+            <Form
+                form={form}
+                onFinish={handleSubmit}
+                autoComplete="off"
+                style={{ background: "white", paddingLeft: "12px" }}
+            >
+                <Form.Item
+                    name={"textData"}
+                    rules={[
+                        {
+                            // required: true,
+                            // message: "Please enter your value!",
+                        },
+                    ]}
+                >
+                    <Input
+                        ref={inputRef}
+                        placeholder="What needs to be done ?"
+                        prefix={allTasks && allTasks.length > 0 ? <DownOutlined onClick={completeAllTasks} /> : ''}
+                        bordered={false}
+                    />
+                </Form.Item>
+            </Form>
+        </>
+    );
+}
+
+export default CreateTask;
